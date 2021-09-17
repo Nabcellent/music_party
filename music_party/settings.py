883627a035bf56a449dc23118a68782733e8a95d
Red-Beard7 <michael.nabangi@strathmore.edu>
2021-09-17 21:20:23 +0300

    Concurrency connections

--- music_party/settings.py
+++ music_party/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'PASSWORD': config("DB_PASSWORD"),
         'HOST': config("DB_HOST"),
         'PORT': config("DB_PORT"),
-        'CONN_MAX_AGE': 0
+        'CONN_MAX_AGE': None
         # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
